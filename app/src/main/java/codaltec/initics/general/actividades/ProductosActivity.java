3b9b38922e05f642090b48468b8c605f414d5e42
Mike Brian Olivera <mikesoft94@hotmail.com>
2017-12-13 09:32:15 -0500

    grilla, adaptador

--- app/src/main/java/codaltec/initics/general/actividades/ProductosActivity.java
+++ app/src/main/java/codaltec/initics/general/actividades/ProductosActivity.java
@@ -1,5 +1,6 @@
 package codaltec.initics.general.actividades;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -27,19 +29,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import codaltec.initics.general.R;
+import codaltec.initics.general.actividades.adapters.PromocionAdapter;
 
 public class ProductosActivity extends AppCompatActivity implements Response.ErrorListener, Response.Listener<JSONObject> {
 
     JsonObjectRequest jsArrayRequest;
     String URL = "http://moi24.org/api/index/";
-    ListView LV;
+
+    GridView GV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
         setTitle("Productos");
-        LV = (ListView) findViewById(R.id.lista_listview);
+        GV = (GridView) findViewById(R.id.list_gridv);
         jsArrayRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 URL,
@@ -62,7 +66,7 @@ public class ProductosActivity extends AppCompatActivity implements Response.Err
         try {
             JSONArray Productos = response.getJSONArray("productos");
             Adapter adaptador = new PromocionAdapter(Productos, this);
-            LV.setAdapter((ListAdapter) adaptador);
+            GV.setAdapter((ListAdapter) adaptador);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -70,55 +74,5 @@ public class ProductosActivity extends AppCompatActivity implements Response.Err
     }
 
 
-    public class PromocionAdapter extends BaseAdapter {
 
-        LayoutInflater inflater;
-        String SERVER;
-
-        Context CTT;
-        JSONArray array_promociones;
-
-
-        public PromocionAdapter(JSONArray array_promociones, Context CTT) {
-            this.CTT = CTT;
-            this.array_promociones = array_promociones;
-            inflater = (LayoutInflater) CTT.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            SERVER = "http://moi24.org";
-        }
-
-
-        @Override
-        public int getCount() {
-            return array_promociones.length();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup parent) {
-            try {
-                if (view == null) {
-                    //view = inflater.from(parent.getContext()).inflate(R.layout.item_promocion, parent, false);
-                    view = inflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
-                }
-
-                JSONObject jsonObject = array_promociones.getJSONObject(i);
-                ((TextView) view.findViewById(android.R.id.text1)).setText(jsonObject.getString("nombre"));
-                //String url_imagen = jsonObject.getString("imagen");
-                //Picasso.with(CTT).load(SERVER + url_imagen).into((ImageView) view.findViewById(R.id.imagen));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return view;
-        }
-
-    }
 }
---
+++ app/src/main/java/codaltec/initics/general/actividades/adapters/PromocionAdapter.java
@@ -0,0 +1,73 @@
+package codaltec.initics.general.actividades.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import codaltec.initics.general.R;
+
+/**
+ * Created by mike on 13/12/17.
+ */
+
+public class PromocionAdapter extends BaseAdapter {
+
+    LayoutInflater inflater;
+    String SERVER;
+
+    Context CTT;
+    JSONArray array_promociones;
+
+
+    public PromocionAdapter(JSONArray array_promociones, Context CTT) {
+        this.CTT = CTT;
+        this.array_promociones = array_promociones;
+        inflater = (LayoutInflater) CTT.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        SERVER = "http://moi24.org";
+    }
+
+
+    @Override
+    public int getCount() {
+        return array_promociones.length();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public View getView(int i, View view, ViewGroup parent) {
+        try {
+            if (view == null) {
+                //view = inflater.from(parent.getContext()).inflate(R.layout.item_promocion, parent, false);
+                view = inflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            }
+
+            JSONObject jsonObject = array_promociones.getJSONObject(i);
+            ((TextView) view.findViewById(android.R.id.text1)).setText(jsonObject.getString("nombre"));
+            //String url_imagen = jsonObject.getString("imagen");
+            //Picasso.with(CTT).load(SERVER + url_imagen).into((ImageView) view.findViewById(R.id.imagen));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+}

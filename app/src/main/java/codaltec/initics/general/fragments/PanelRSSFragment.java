4abaae49b6c5592f697ad798660713c93d60e9e5
Mike Brian Olivera <mikesoft94@hotmail.com>
2017-12-18 11:33:05 -0500

    accion de boton en fragment

--- app/src/main/java/codaltec/initics/general/fragments/PanelRSSFragment.java
+++ app/src/main/java/codaltec/initics/general/fragments/PanelRSSFragment.java
@@ -1,6 +1,7 @@
 package codaltec.initics.general.fragments;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,14 +10,18 @@ import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import codaltec.initics.general.MainActivity;
 import codaltec.initics.general.R;
 import codaltec.initics.general.basedatos.BaseDatos;
+import es.dmoral.toasty.Toasty;
 
 
 public class PanelRSSFragment extends Fragment {
@@ -53,5 +58,40 @@ public class PanelRSSFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_panel_rss,menu);
+    }
+
+
+    Dialog DDD;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.add_rss){
+            DDD = new Dialog(MA);
+            DDD.setTitle("lalalala");
+            DDD.setContentView(R.layout.menu_crear_rss);
+            final EditText nombre=((EditText)DDD.findViewById(R.id.input_nombre));
+            final EditText link=((EditText)DDD.findViewById(R.id.input_link));
+            ((Button)DDD.findViewById(R.id.btn_save_rss)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if( nombre.getText().toString().length()<2 || link.getText().toString().length()<5){
+                        Toasty.error(MA,"Escribe bien los campos", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    if(DB.agregarFeed(nombre.getText().toString(),link.getText().toString())){
+                        Toasty.success(MA,"RSS agregado",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+
+            DDD.show();
+
+
+        }
+
+        return  true;
     }
 }

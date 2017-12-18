ac3e1d1bef8a439cbc13e53c3b21a3880dc30277
Mike Brian Olivera <mikesoft94@hotmail.com>
2017-12-18 09:38:43 -0500

    DB

--- app/src/main/java/codaltec/initics/general/basedatos/BaseDatos.java
+++ app/src/main/java/codaltec/initics/general/basedatos/BaseDatos.java
@@ -1,6 +1,8 @@
 package codaltec.initics.general.basedatos;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,10 +15,10 @@ public class BaseDatos extends SQLiteOpenHelper {
     public final static String nombre = "nombre";
     public final static String link = "link";
     public final static String type_text = "text";
-
     public BaseDatos(Context context) {
-        super(context, "rss.sb", null, 1);
+        super(context, "rss.db", null, 1);
     }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(
@@ -27,4 +29,29 @@ public class BaseDatos extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+
+    public boolean agregarFeed(String nombre, String URL){
+        try {
+            ContentValues CV=new ContentValues();
+            CV.put("link",URL);
+            CV.put("nombre",nombre);
+            getWritableDatabase().insert("paginas",null,CV);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Cursor obtenerFeeds(){
+        return getWritableDatabase().rawQuery("select * from paginas",null);
+    }
+
+    public Cursor obtenerFeeds(int posicion){
+        Cursor C=getWritableDatabase().rawQuery("select * from paginas",null);
+        C.move(posicion+1);
+        return C;
+    }
+
 }
